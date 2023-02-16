b257f6ae7bbf2967d3dce58f388ed158dd2b0c19
vivek2293 <vivek22935@gmail.com>
2023-02-16 10:31:11 +0530

    PORT to 8000

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use("/api/v1", routes);
 
 //Starting server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
 });
